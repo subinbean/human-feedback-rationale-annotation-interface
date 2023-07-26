fbe6decb523f9a682a6393136ef95e48ea76ce04
Subin Lee <subin1719@gmail.com>
2023-07-26 12:49:29 -0400

    draft of initial setup

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -1,20 +1,29 @@
-import React from 'react'
-import {Navbar, Container} from 'react-bootstrap';
-import logo from '../upenn.png'
+import React from "react";
+import { Navbar, Container } from "react-bootstrap";
+import logo from "../upenn.png";
 
 const navbar = () => {
-    return (
-        <Navbar bg="light"> 
-        <Container>
-            <Navbar.Brand>
-                <div style={{display: 'flex', alignItems: 'center'}}>
-                <img src={logo} width="50" height="30" alt="UPenn logo" style={{marginLeft: ''}}/>
-                <div style={{marginLeft: "-2px"}}> Expert Evaluation of AI Answers </div>
-                </div>
-            </Navbar.Brand>
-        </Container>
-        </Navbar>
-    )
-}
+  return (
+    <Navbar bg="light">
+      <Container>
+        <Navbar.Brand>
+          <div style={{ display: "flex", alignItems: "center" }}>
+            <img
+              src={logo}
+              width="50"
+              height="30"
+              alt="UPenn logo"
+              style={{ marginLeft: "" }}
+            />
+            <div style={{ marginLeft: "-2px" }}>
+              {" "}
+              Providing Feedback for AI Outputs{" "}
+            </div>
+          </div>
+        </Navbar.Brand>
+      </Container>
+    </Navbar>
+  );
+};
 
-export default navbar
+export default navbar;
--- frontend/src/pages/WelcomePage.js
+++ frontend/src/pages/WelcomePage.js
@@ -2,34 +2,35 @@ import { useState } from "react";
 import "./pagesStyle.css";
 import { useNavigate } from "react-router-dom";
 import { Card, Button, Form, Alert } from "react-bootstrap";
-import axios from "axios";
+// import axios from "axios";
 
 const WelcomePage = () => {
   const navigate = useNavigate();
   const [prolificId, setProlificId] = useState("");
-  const [alert, setAlert] = useState(false);
-  const baseUrl = `/api/questions/${prolificId}`;
+  // const [alert, setAlert] = useState(false);
+  // const baseUrl = `/api/questions/${prolificId}`;
 
   const onClick = (e) => {
     e.preventDefault();
-    axios
-      .get(baseUrl)
-      .then((response) => {
-        if (response.data.length === 0) {
-          setAlert(true);
-        } else {
-          const todoQuestions = response.data.filter(
-            (question) => !question.completed
-          );
-          console.log(todoQuestions);
-          if (todoQuestions.length === 0) {
-            navigate("/submission");
-          } else {
-            navigate("/questions", { state: { data: todoQuestions } });
-          }
-        }
-      })
-      .catch((error) => console.log(error));
+    navigate("/questions");
+    // axios
+    //   .get(baseUrl)
+    //   .then((response) => {
+    //     if (response.data.length === 0) {
+    //       setAlert(true);
+    //     } else {
+    //       const todoQuestions = response.data.filter(
+    //         (question) => !question.completed
+    //       );
+    //       console.log(todoQuestions);
+    //       if (todoQuestions.length === 0) {
+    //         navigate("/submission");
+    //       } else {
+    //         navigate("/questions", { state: { data: todoQuestions } });
+    //       }
+    //     }
+    //   })
+    //   .catch((error) => console.log(error));
   };
 
   const renderAlert = () => {
@@ -37,8 +38,7 @@ const WelcomePage = () => {
       return (
         <Alert
           variant="danger"
-          style={{ width: "50rem", marginTop: "20px", textAlign: "left" }}
-        >
+          style={{ width: "50rem", marginTop: "20px", textAlign: "left" }}>
           {" "}
           No questions match the provided ID in the database{" "}
         </Alert>
@@ -47,112 +47,51 @@ const WelcomePage = () => {
   };
 
   return (
-    <div align="center">
-      <Card style={{ width: "50rem", marginTop: "20px" }}>
+    <div className="welcome">
+      <Card style={{ width: "40rem", height: "30rem", marginTop: "12rem" }}>
         <Card.Body>
-          <Card.Title>
+          <Card.Title style={{ textAlign: "center", marginTop: "1rem" }}>
             {" "}
-            <b> Stage 2: Detailed Instructions </b>{" "}
+            <b> User Study - Providing Feedback for AI Outputs </b>{" "}
           </Card.Title>
-          <Card.Text style={{ textAlign: "left" }}>
+          <Card.Text
+            style={{
+              textAlign: "left",
+              lineHeight: "1.7",
+              marginTop: "2rem",
+            }}>
             <p>
               {" "}
               Thank you for your interest in our task! We are a group of
               researchers at the University of Pennsylvania conducting a study
-              to understand how experts from various fields use AI / large
-              language models in information-seeking scenarios. We are
-              particularly interested in evaluating the accuracy and factual
-              correctness of answers produced by such systems. We are inviting
-              participants who are professionals / experts in these fields:{" "}
+              to understand how to present answers from AI systems / language
+              models, such that they are interpretable, easy to provide human
+              feedback to and trustworthy for end users. We are inviting
+              participants who [are experts in the medicine / healthcare fields
+              and] are interested in helping build more reliable AI systems that
+              can improve through their interactions with users.{" "}
             </p>
-            <p>
-              {" "}
-              [Anthropology / Architecture / Biology / Business / Chemistry /
-              Classical Studies / Criminology / Culinary Arts / Environmental
-              Science / Economics / Education / Engineering and Technology /
-              Geography / History / Journalism / Law / Linguistics / Literature
-              / Mathematics / Medicine / Music / Philosophy / Physics and
-              Astronomy / Political Science / Psychology / Theology / Sociology
-              / Visual Arts]{" "}
+            <p style={{ marginTop: "4rem" }}>
+              Please enter your prolific ID down below to begin the task:
+              <Form
+                style={{ marginTop: "10px", width: "400px" }}
+                onSubmit={onClick}>
+                <Form.Group className="mb-3">
+                  <Form.Control
+                    placeholder="Enter Prolific ID"
+                    onChange={(text) => setProlificId(text.target.value)}
+                  />
+                </Form.Group>
+              </Form>
             </p>
-            <p>
-              {" "}
-              The study will proceed in two stages and we would request you in
-              both stages.{" "}
-            </p>
-            <p>
-              <ol>
-                <li>
-                  Question Writing: We will ask you to write a question from
-                  your domain.
-                </li>
-                <li>
-                  Answer Validation and Revision: We will show you an answer
-                  produced by an AI system, and ask you to validate different
-                  aspects of this answer. We will then ask you to revise this
-                  answer to be factually correct and well-supported with
-                  citations.
-                </li>
-              </ol>
-            </p>
-            <p>
-              {" "}
-              The current task is the <b> stage 2 </b> of the study.{" "}
-            </p>
-            <p>
-              <b>
-                {" "}
-                <i> Note about completion time: </i>
-              </b>{" "}
-              Note that we have made a best estimate for how long it should take
-              to complete this task, based on a small number of participants.
-              However, the time spent can vary across participants and across
-              questions. If you end up spending more time than the allocated
-              time, please feel free to let us know and we would be happy to
-              bonus you for the extra time spent. Please prioritize{" "}
-              <b> quality </b> and do not rush through the task. You will get a
-              completion code after you finish annotating all 3 questions.{" "}
-            </p>
-            <p>
-              {" "}
-              <font color="maroon">
-                {" "}
-                <b>
-                  **Before moving on, please watch the following instruction
-                  video for this task{" "}
-                  <a
-                    href="https://www.loom.com/share/29690d5feb9b4663b77e2dc807a6b0b9?sid=2b00e524-3f02-4c60-a260-73cb32e9b15e"
-                    target="_blank"
-                    rel="noreferrer noopener"
-                  >
-                    {" "}
-                    here
-                  </a>{" "}
-                  **{" "}
-                </b>{" "}
-              </font>{" "}
-            </p>
-            Please enter your prolific ID down below to begin task 2:
-            <Form
-              style={{ marginTop: "10px", width: "400px" }}
-              onSubmit={onClick}
-            >
-              <Form.Group className="mb-3">
-                <Form.Control
-                  placeholder="Enter Prolific ID"
-                  onChange={(text) => setProlificId(text.target.value)}
-                />
-              </Form.Group>
-            </Form>
           </Card.Text>
         </Card.Body>
       </Card>
-      {renderAlert()}
+      {/* {renderAlert()} */}
       <Button
         variant="outline-primary"
         onClick={onClick}
-        style={{ marginTop: "20px" }}
-      >
+        style={{ marginTop: "20px" }}>
         {" "}
         Submit and start task{" "}
       </Button>
